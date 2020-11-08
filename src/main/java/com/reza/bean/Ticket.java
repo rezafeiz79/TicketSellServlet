fa1c00e2284414ced7cb09c6be72442c794d3c82
rezafeiz79 <rezafeiz79@gmail.com>
2020-11-08 16:42:34 +0330

    written ticket bean

--- src/main/java/com/reza/bean/Ticket.java
+++ src/main/java/com/reza/bean/Ticket.java
@@ -1,4 +1,45 @@
 package com.reza.bean;
 
+import javax.persistence.*;
+
+@Entity
 public class Ticket {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    @ManyToOne
+    Trip trip;
+    @ManyToOne
+    Customer customer;
+
+    public Ticket(Integer id, Trip trip, Customer customer) {
+        this.id = id;
+        this.trip = trip;
+        this.customer = customer;
+    }
+
+    public Ticket() { }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Trip getTrip() {
+        return trip;
+    }
+
+    public void setTrip(Trip trip) {
+        this.trip = trip;
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
 }
