24097e7ee30b4a97e9c7d8482290b7ff050d70b0
rezafeiz79 <rezafeiz79@gmail.com>
2020-11-08 16:12:22 +0330

    written customer bean

--- src/main/java/com/reza/bean/Customer.java
+++ src/main/java/com/reza/bean/Customer.java
@@ -1,4 +1,66 @@
 package com.reza.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Customer {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String name;
+    String gender;
+    String username;
+    String password;
+
+    public Customer(Integer id, String name, String gender, String username, String password) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Customer() { }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
