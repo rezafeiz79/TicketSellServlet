333d1a0ffe1018c19eed5f0cd2605f8bfa0b9fc1
rezafeiz79 <rezafeiz79@gmail.com>
2020-11-08 16:31:25 +0330

    written trip bean

--- src/main/java/com/reza/bean/Trip.java
+++ src/main/java/com/reza/bean/Trip.java
@@ -1,4 +1,69 @@
 package com.reza.bean;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
 public class Trip {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Integer id;
+    String originCity;
+    String destinationCity;
+    LocalTime moveTime;
+    LocalDate moveDate;
+
+    public Trip(Integer id, String originCity, String destinationCity, LocalTime moveTime, LocalDate moveDate) {
+        this.id = id;
+        this.originCity = originCity;
+        this.destinationCity = destinationCity;
+        this.moveTime = moveTime;
+        this.moveDate = moveDate;
+    }
+
+    public Trip() { }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getOriginCity() {
+        return originCity;
+    }
+
+    public void setOriginCity(String originCity) {
+        this.originCity = originCity;
+    }
+
+    public String getDestinationCity() {
+        return destinationCity;
+    }
+
+    public void setDestinationCity(String destinationCity) {
+        this.destinationCity = destinationCity;
+    }
+
+    public LocalTime getMoveTime() {
+        return moveTime;
+    }
+
+    public void setMoveTime(LocalTime moveTime) {
+        this.moveTime = moveTime;
+    }
+
+    public LocalDate getMoveDate() {
+        return moveDate;
+    }
+
+    public void setMoveDate(LocalDate moveDate) {
+        this.moveDate = moveDate;
+    }
 }
+
