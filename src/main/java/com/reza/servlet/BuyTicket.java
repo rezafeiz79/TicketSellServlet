3d785fbce385dcec69ba9ac0cae7b13d23296aec
rezafeiz79 <rezafeiz79@gmail.com>
2020-11-09 19:28:15 +0330

    edited BuyTicket servlet

--- src/main/java/com/reza/servlet/BuyTicket.java
+++ src/main/java/com/reza/servlet/BuyTicket.java
@@ -1,4 +1,24 @@
 package com.reza.servlet;
 
-public class BuyTicket {
+import com.reza.bean.Customer;
+import com.reza.bean.Ticket;
+import com.reza.bean.Trip;
+import com.reza.util.DatabaseUtil;
+import org.hibernate.Session;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BuyTicket extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Session session = DatabaseUtil.getSessionFactory().openSession();
+        Trip trip = DatabaseUtil.getEntityById(session, Integer.parseInt(req.getParameter("tripId")), Trip.class);
+        Ticket ticket = new Ticket(null, trip, (Customer) req.getSession().getAttribute("customer"));
+        DatabaseUtil.saveEntity(session, ticket);
+
+    }
 }
